33145aca38065c93a300f0d8175bba35081bad5f
bjorntheemperor <erasusdk@gmail.com>
2021-05-21 19:39:00 +0300

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,51 +1,66 @@
 console.log("Hello!");
 document.cookie = "user=Kim"
 console.log(document.cookie);
-/**
- * Get the user IP throught the webkitRTCPeerConnection
- * @param onNewIP {Function} listener function to expose the IP locally
- * @return undefined
- */
-function getUserIP(onNewIP) { //  onNewIp - your listener function for new IPs
-    //compatibility for firefox and chrome
-    var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-    var pc = new myPeerConnection({
-        iceServers: []
-    }),
-    noop = function() {},
-    localIPs = {},
-    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
-    key;
+function old() {
+  /**
+   * Get the user IP throught the webkitRTCPeerConnection
+   * @param onNewIP {Function} listener function to expose the IP locally
+   * @return undefined
+   */
+  function getUserIP(onNewIP) { //  onNewIp - your listener function for new IPs
+      //compatibility for firefox and chrome
+      var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+      var pc = new myPeerConnection({
+          iceServers: []
+      }),
+      noop = function() {},
+      localIPs = {},
+      ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+      key;
 
-    function iterateIP(ip) {
-        if (!localIPs[ip]) onNewIP(ip);
-        localIPs[ip] = true;
-    }
+      function iterateIP(ip) {
+          if (!localIPs[ip]) onNewIP(ip);
+          localIPs[ip] = true;
+      }
 
-     //create a bogus data channel
-    pc.createDataChannel("");
+       //create a bogus data channel
+      pc.createDataChannel("");
 
-    // create offer and set local description
-    pc.createOffer().then(function(sdp) {
-        sdp.sdp.split('\n').forEach(function(line) {
-            if (line.indexOf('candidate') < 0) return;
-            line.match(ipRegex).forEach(iterateIP);
-        });
+      // create offer and set local description
+      pc.createOffer().then(function(sdp) {
+          sdp.sdp.split('\n').forEach(function(line) {
+              if (line.indexOf('candidate') < 0) return;
+              line.match(ipRegex).forEach(iterateIP);
+          });
 
-        pc.setLocalDescription(sdp, noop, noop);
-    }).catch(function(reason) {
-        // An error occurred, so handle the failure to connect
-    });
+          pc.setLocalDescription(sdp, noop, noop);
+      }).catch(function(reason) {
+          // An error occurred, so handle the failure to connect
+      });
 
-    //listen for candidate events
-    pc.onicecandidate = function(ice) {
-        if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
-        ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
-    };
+      //listen for candidate events
+      pc.onicecandidate = function(ice) {
+          if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+          ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+      };
+  }
+
+  // Usage
+
+  getUserIP(function(ip){
+      alert("Got IP! :" + ip);
+  });
+
 }
 
-// Usage
+function json(url) {
+  return fetch(url).then(res => res.json());
+}
 
-getUserIP(function(ip){
-    alert("Got IP! :" + ip);
+let apiKey = 'd8a8e4535e5b1518f376f7548c3d64bb540c0546dc1e72b4c509c435';
+json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
+  console.log(data.ip);
+  console.log(data.city);
+  console.log(data.country_code);
+  // so many more properties
 });
