dceaaa44b114929ac1145d17d9eae38baeb8c90e
bjorntheemperor <erasusdk@gmail.com>
2021-05-22 02:26:52 +0300

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -1,58 +1,4 @@
 
-document.cookie = "user=Kim"
-console.log(document.cookie);
-function old() {
-  /**
-   * Get the user IP throught the webkitRTCPeerConnection
-   * @param onNewIP {Function} listener function to expose the IP locally
-   * @return undefined
-   */
-  function getUserIP(onNewIP) { //  onNewIp - your listener function for new IPs
-      //compatibility for firefox and chrome
-      var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-      var pc = new myPeerConnection({
-          iceServers: []
-      }),
-      noop = function() {},
-      localIPs = {},
-      ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
-      key;
-
-      function iterateIP(ip) {
-          if (!localIPs[ip]) onNewIP(ip);
-          localIPs[ip] = true;
-      }
-
-       //create a bogus data channel
-      pc.createDataChannel("");
-
-      // create offer and set local description
-      pc.createOffer().then(function(sdp) {
-          sdp.sdp.split('\n').forEach(function(line) {
-              if (line.indexOf('candidate') < 0) return;
-              line.match(ipRegex).forEach(iterateIP);
-          });
-
-          pc.setLocalDescription(sdp, noop, noop);
-      }).catch(function(reason) {
-          // An error occurred, so handle the failure to connect
-      });
-
-      //listen for candidate events
-      pc.onicecandidate = function(ice) {
-          if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
-          ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
-      };
-  }
-
-  // Usage
-
-  getUserIP(function(ip){
-      alert("Got IP! :" + ip);
-  });
-
-}
-
 function json(url) {
   return fetch(url).then(res => res.json());
 }
@@ -63,6 +9,8 @@ json(`https://api.ipdata.co?api-key=${apiKey}`).then(data => {
   console.log(data.city);
   console.log(data.country_code);
   document.cookie = "ip="+data.ip;
+  document.cookie = "city="+data.city;
+  document.cookie = "country="+data.country_code;
   // so many more properties
 });
 
@@ -85,4 +33,3 @@ async function handleSubmit(event) {
   console.log(event.target.action);
 }
 form.addEventListener("submit", handleSubmit)
-console.log('hello allo!!');
