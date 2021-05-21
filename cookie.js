86fb6318f53b0a734d92347db99d5519fdb05a11
bjorntheemperor <erasusdk@gmail.com>
2021-05-22 01:40:30 +0300

    Update cookie.js

--- cookie.js
+++ cookie.js
@@ -71,6 +71,7 @@ var form = document.getElementById("my-form");
 async function handleSubmit(event) {
   event.preventDefault();
   var data = new FormData(event.target);
+  data.append('username', 'Sam')
   console.log(data);
   fetch(event.target.action, {
     method: form.method,
